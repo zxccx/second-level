3405b88a9583cc9a18f61d90fb942ace5631c83a
崔北虹 <858739100@qq.com>
2016-09-23 17:13:34 +0800

    add project

---
+++ second leve/second leve/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  second leve
+//
+//  Created by C on 16/9/23.
+//  Copyright © 2016年 com.zhangyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ second leve/second leve/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  second leve
+//
+//  Created by C on 16/9/23.
+//  Copyright © 2016年 com.zhangyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
